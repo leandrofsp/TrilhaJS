b1d9a17216422ee87a0ea1906493855bd8c3eaf0
Leandro Silva <leandrof.sp@gmail.com>
2017-02-17 16:28:39 -0200

    ex aula 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import upperText from '../util/util';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/style.scss';
+import Home from './components/app';
+
+ReactDOM.render(<Home />, document.getElementById('app'));
 
 console.log(upperText.upper('trilha js'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
 	entry: './src/index.js',
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'build')
-	}
+	},
+		module: {
+		rules: [
+			{
+				test:/\.scss$/,
+				use:ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+			},
+			{
+				test:/\.js$/,
+				use: 'babel-loader'
+			}
+		]
+	},
+	plugins:[
+	new ExtractTextPlugin('style.css')
+	]
 }
 
 module.exports = config;
